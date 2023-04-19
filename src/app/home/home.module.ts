ca581b5a2817dcdc0aeb1b4987795ed893a228c8
Maria Paula Mosquera Rengifo <maria.mosquera@grupokonecta.com>
2023-04-18 20:32:57 -0500

    add project structure: folders, modules, components, guard

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { MoviesComponent } from './components/movies/movies.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
+
+
+@NgModule({
+  declarations: [
+    MoviesComponent,
+    MovieDetailComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
