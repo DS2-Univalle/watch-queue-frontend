4e6b7156dee6ea6c8358e01cf124d68d706d2b55
Maria Paula Mosquera Rengifo <maria.mosquera@grupokonecta.com>
2023-04-26 00:06:36 -0500

    add initial sign up interface

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,12 +4,18 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { CardLoginComponent } from './components/card-login/card-login.component';
+import { CardSignupComponent } from './components/card-signup/card-signup.component';
+import { BackgroundComponent } from './components/background/background.component';
 
 
 @NgModule({
   declarations: [
     SigninComponent,
-    SignupComponent
+    SignupComponent,
+    CardLoginComponent,
+    CardSignupComponent,
+    BackgroundComponent
   ],
   imports: [
     CommonModule,
